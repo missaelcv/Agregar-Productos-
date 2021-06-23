ca4f7dca9fae0fa281dbe1cb72e111b5b771ca1e
missaelcv <missaelcv0220@gmail.com>
2021-06-22 22:58:11 -0400

    Tipografía,colores,espaciados(margenes/padding) y sombra

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,32 @@
 <template>
     <q-page padding>
-        <h1>Hola About</h1>
+        <h3>Hola About</h3>
+
+            <h6 class="text- primary bg-primary">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Maxime sint voluptatem ad voluptatibus. Ut aspernatur ipsam aut totam,
+            quasi dignissimos odit deleniti repellendus nobis laboriosam doloribus
+            nesciunt. Sequi, nostrum architecto.</h6>
+
+             <h6 class="shadow-5"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Maxime sint voluptatem ad voluptatibus. Ut aspernatur ipsam aut totam,
+            quasi dignissimos odit deleniti repellendus nobis laboriosam doloribus
+            nesciunt. Sequi, nostrum architecto.</h6>
+
+             <h6 class="text-green-8 q-mt-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Maxime sint voluptatem ad voluptatibus. Ut aspernatur ipsam aut totam,
+            quasi dignissimos odit deleniti repellendus nobis laboriosam doloribus
+            nesciunt. Sequi, nostrum architecto.</h6>
+
+             <h6 class="text-accent">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Maxime sint voluptatem ad voluptatibus. Ut aspernatur ipsam aut totam,
+            quasi dignissimos odit deleniti repellendus nobis laboriosam doloribus
+            nesciunt. Sequi, nostrum architecto.</h6>
+
+             <h6 class="text-red-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Maxime sint voluptatem ad voluptatibus. Ut aspernatur ipsam aut totam,
+            quasi dignissimos odit deleniti repellendus nobis laboriosam doloribus
+            nesciunt. Sequi, nostrum architecto.</h6>
+
+           
         </q-page>
 </template>

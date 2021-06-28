4129675b5aa8834facd24d8f7e1596ba84dbb0d7
missaelcv <missaelcv0220@gmail.com>
2021-06-27 20:40:01 -0400

    producto.valuel propiedad

--- src/components/PintarDatos.vue
+++ src/components/PintarDatos.vue
@@ -1,6 +1,59 @@
 <template>
+
+ <pre>{{Productos}}</pre>
+
      <q-table
-            title="Produtos"
+     class="q-mt-md"
+     no-data-label="sin productos a la vista"
+     title="Productos"
+     :columns="columns"
+     :rows="productos"
 
      />
 </template>
+
+<script>
+
+const columns = [
+
+     {
+    name: 'producto',
+    required: true,
+    label: 'Producto',
+    align: 'left',
+    field: 'producto',
+    sortable: true
+     },
+
+     {
+    name: 'prioridad',
+    required: true,
+    label: 'Prioridad',
+    align: 'left',
+    field: 'prioridad',
+    sortable: true
+     }
+]
+
+const rows = [
+     {
+          producto: 'producto 1',
+          prioridad: 'Maxima'
+     }
+]
+
+
+export default ({
+     props: {
+          productos: Array
+     },
+     setup() {
+          return{
+
+          
+          columns,
+          rows
+          }
+     },
+})
+</script>
